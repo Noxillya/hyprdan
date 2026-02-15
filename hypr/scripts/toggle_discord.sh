d58ef842089b0f025131397932d72cf16fe126f6
Noxillya <jeong.dan@pm.me>
2026-02-15 17:08:09 +0100

    Changement de lecteur de fichier

--- hypr/scripts/toggle_discord.sh
+++ hypr/scripts/toggle_discord.sh
@@ -1,41 +1,40 @@
 #!/usr/bin/env bash
 
-# Nom du special workplace
 WS_NAME="discord"
 
-# Fonction pour trouver la fenetre discord
 get_discord_addr() {
-	hyprctl clients -j | jq -r '
-		.[]
-		| select((.class // "") | test("(?i)discord"))
-		| .address
-	'	| head -n1
+  hyprctl clients -j | jq -r '
+    .[]
+    | select(
+        ((.class // "") | test("(?i)^discord$"))
+        or
+        (((.class // "") | test("(?i)^kitty$")) and ((.title // "") | test("(?i)^discord$")))
+      )
+    | .address
+  ' | head -n1
 }
 
-DISCORD_ADDR=$(get_discord_addr)
+discord_ADDR=$(get_discord_addr)
 
-# Si aucune fenetre Discord, on lance discord
-if [ -z "$DISCORD_ADDR" ]; then
-	if ! pgrep -x "discord" >/dev/null && ! pgrep -f "com.discordapp.Discord" >/dev/null; then
-		discord & disown
-	fi
+# Si aucune fenêtre discord, on lance discord
+if [ -z "$discord_ADDR" ]; then
+  if ! pgrep -x "discord" >/dev/null; then
+    discord & disown
+  fi
 
-	#On attend que la fenetre apparaisse
-	for i in {1..50};do
-		sleep 0.1
-		DISCORD_ADDR=$(get_discord_addr)
-		[ -n "$DISCORD_ADDR" ] && break
-	done
+  # Attente apparition fenêtre
+  for i in {1..80}; do
+    sleep 0.1
+    discord_ADDR=$(get_discord_addr)
+    [ -n "$discord_ADDR" ] && break
+  done
 fi
 
-# Si on a une fenetre discord, on la met dans le special workspace
-if [ -c "$DISCORD_ADDR" ]; then
-	hyprctl dispatch movetoworkspacesilent "special:$WS_NAME, address:$DISCORD_ADDR"
+# Si on a une fenêtre discord, on la met dans le special workspace
+if [ -n "$discord_ADDR" ]; then
+  hyprctl dispatch movetoworkspacesilent "special:$WS_NAME, address:$discord_ADDR"
 fi
 
-# Enfin on toggle le special workspace
+# Toggle du special workspace
 hyprctl dispatch togglespecialworkspace "$WS_NAME"
-
-
-
 
